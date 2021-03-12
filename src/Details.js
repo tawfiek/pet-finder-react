52d16ea3d2662befadf1523bfab56ceed69c1d46
Tawfiek <tawfiek.108@gmail.com>
2021-03-12 17:24:43 +0200

    feat: implement details component using react class component

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,45 @@
-import { useParams } from "react-router-dom";
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
 
-const Details = () => {
-  const { id } = useParams();
-  return <h2>{id}</h2>;
-};
+class Details extends Component {
+  state = { loading: true };
 
-export default Details;
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading … </h2>;
+    }
+
+    const {
+      animal,
+      breed,
+      city,
+      state,
+      description,
+      name,
+      images,
+    } = this.state;
+
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
